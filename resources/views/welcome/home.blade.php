a8988d3cbf268c9c18eb499470db5ab9643aec90
Eluk-Samuel-Kiira <samuelkiiraeluk@gmail.com>
2024-11-05 10:54:34 +0300

    Final changes

--- resources/views/welcome/home.blade.php
+++ resources/views/welcome/home.blade.php
@@ -308,7 +308,7 @@
                 <div class="row g-4">
                     @foreach ($services as $service)
                         <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                            <a class="service-item rounded" href="">
+                            <div class="room-item shadow rounded overflow-hidden">
                                 <div class="position-relative">
                                     <img class="img-fluid" 
                                         src="{{ isset($service->service_icon) ? asset('storage/' . $service->serviceImagae->image) : asset('hotelier/img/room-1.jpg') }}" 
@@ -316,12 +316,23 @@
                                     <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">{{ appDefaultCurrency() }} {{ $room->price }}/Night</small>
                                 </div>
                                 <div class="p-4 mt-2">
-                                <div class="d-flex flex-column mb-3">
-                                    <h5 class="mb-0">{{ ucwords(str_replace('_', ' ', $service->name)) }}</h5>
+                                    <div class="d-flex justify-content-between mb-3">
+                                        <h5 class="mb-0">{{ ucwords(str_replace('_', ' ', $service->name)) }}</h5>
+                                        <div class="ps-2">
+                                            <small class="fa fa-star text-primary"></small>
+                                            <small class="fa fa-star text-primary"></small>
+                                            <small class="fa fa-star text-primary"></small>
+                                            <small class="fa fa-star text-primary"></small>
+                                            <small class="fa fa-star text-primary"></small>
+                                        </div>
+                                    </div>
                                     <p class="text-body mb-3">{{ $service->narration }}</p>
+                                    <div class="d-flex justify-content-between">
+                                        {{--<a class="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a> --}}
+                                        <a class="btn btn-sm btn-dark rounded py-2 px-4" href="#book_now">Book Now</a>
+                                    </div>
                                 </div>
-                                </div>
-                            </a>
+                            </div>
                         </div>
                     @endforeach
 
